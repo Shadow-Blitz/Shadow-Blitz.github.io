6774028afb8f6fa3c3a3208967d4032e72541602
Shadow Blitz <skyedexin@gmail.com>
2018-09-22 09:23:19 +1000

    Saturday Morning

--- js/about_me.js
+++ js/about_me.js
@@ -1,6 +1,6 @@
 deg = 0
 var tick = () => {
-    document.querySelector('body').style.background = `background linear-gradient(${deg += .5}deg, #26B1CC, #ec448c) fill`
+    document.querySelector('body').style.background = `background linear-gradient(${deg += .1}deg, #26B1CC, #ec448c) fill`
     requestAnimationFrame(tick)
 }
 requestAnimationFrame(tick)
