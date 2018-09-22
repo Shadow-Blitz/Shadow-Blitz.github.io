8060c4fdb504595456af525f46f590172bc30adf
Shadow Blitz <skyedexin@gmail.com>
2018-09-22 12:07:21 +1000

    Saturday Midday and about me done

--- js/about_me.js
+++ js/about_me.js
@@ -1,6 +1,6 @@
 deg = 0
 var tick = () => {
-    document.querySelector('body').style.background = `background linear-gradient(${deg += .1}deg, #26B1CC, #ec448c) fill`
+    document.querySelector('body').style.background = `background linear-gradient(${deg += 0.1}deg, #26B1CC, #ec448c)`
     requestAnimationFrame(tick)
 }
 requestAnimationFrame(tick)
--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 deg = 0
 var tick = () => {
-    document.querySelector('body').style.background = `background linear-gradient(${deg += .5}deg, #26B1CC, #ec448c)`
+    document.querySelector('body').style.background = `background linear-gradient(${deg += .9}deg, #26B1CC, #ec448c)`
     requestAnimationFrame(tick)
 }
 requestAnimationFrame(tick)
