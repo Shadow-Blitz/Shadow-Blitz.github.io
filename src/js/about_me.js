4572c506d6e82b2ea7b3836b3bfb7057835eeb88
JMZ10 <jblaik74@gmail.com>
2019-09-07 12:51:08 +1000

    fixed gradients on about me page

--- src/js/about_me.js
+++ src/js/about_me.js
@@ -1,6 +1,7 @@
 deg = 0
 var tick = () => {
     document.querySelector('body').style.background = `background linear-gradient(${deg += 0.1}deg, #26B1CC, #ec448c)`
+    document.querySelector('html, body').style.backgroundSize = `cover`
     requestAnimationFrame(tick)
 }
 requestAnimationFrame(tick)
--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 deg = 0
 var tick = () => {
     document.querySelector('body').style.background = `linear-gradient(${deg += .9}deg, #26B1CC, #ec448c)`
-    document.querySelector('body').style.backgroundSize = `cover`
+    document.querySelector('html, body').style.backgroundSize = `cover`
     requestAnimationFrame(tick)
 
 }
